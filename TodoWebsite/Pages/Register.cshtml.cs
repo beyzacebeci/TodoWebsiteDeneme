3d8554dc26b331e5e20453761dfa893c81d13998
bengisu <bengisucalskan@gmail.com>
2023-05-22 20:14:14 +0300

    loginb2

--- TodoWebsite/Pages/Register.cshtml.cs
+++ TodoWebsite/Pages/Register.cshtml.cs
@@ -7,13 +7,9 @@ namespace TodoWebsite.Pages
 {
     public class RegisterModel : PageModel
     {
-        private readonly ILogger<RegisterModel> _logger;
+    
         public List<UniversityModel> universityList;
 
-        public RegisterModel(ILogger<RegisterModel> logger)
-        {
-            
-        }
 
         [BindProperty]
         public string country { get; set; }
