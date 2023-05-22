fa1fb35f460359e218f08a68767d996503ae29fe
Beyza Cebeci <beyzacebeci34@gmail.com>
2023-05-22 20:41:28 +0300

    jkkjjk

--- TodoWebsite/Pages/Register.cshtml.cs
+++ TodoWebsite/Pages/Register.cshtml.cs
@@ -1,19 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using TodoWebsite.Models;
-
+using System;
 
 namespace TodoWebsite.Pages
 {
     public class RegisterModel : PageModel
     {
-    
-        public List<UniversityModel> universityList;
-
+        [BindProperty]
+        public string Name { get; set; }
 
         [BindProperty]
-        public string country { get; set; }
+        public string Surname { get; set; }
 
-     
+        [BindProperty]
+        public string Username { get; set; }
+
+        [BindProperty]
+        public string NewPassword { get; set; }
+
+        [BindProperty]
+        public string NewPasswordAgain { get; set; }
+
+        public void OnPost()
+        {
+            
+            Console.WriteLine(Name);
+            Console.WriteLine(Surname);
+            Console.WriteLine(Username);
+            Console.WriteLine(NewPassword);
+            Console.WriteLine(NewPasswordAgain);
+
+       
+        }
     }
 }
