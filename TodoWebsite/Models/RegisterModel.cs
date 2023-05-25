ae9b2c855f841563719f57c31ae6b107fe74561f
Beyza Cebeci <beyzacebeci34@gmail.com>
2023-05-25 17:37:16 +0300

    feat: add registercontroller

---
+++ TodoWebsite/Models/RegisterModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoWebsite.Models
+{
+    public interface RegisterModel
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
