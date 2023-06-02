7605ccbf53c0d3244686192866ad7f4c1cc6fc84
Selim <omer.gul@fimple.co.uk>
2023-06-02 17:29:19 +0300

    feat : add http request base class

--- TodoWebsite/Pages/Home.cshtml.cs
+++ TodoWebsite/Pages/Home.cshtml.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +5,43 @@ namespace TodoWebsite.Pages.Shared
 {
     public class HomeModel : PageModel
     {
-        public void OnGet()
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HomeModel(IHttpContextAccessor _httpContextAccessor)
         {
+            this._httpContextAccessor = _httpContextAccessor;
+
+
+
+            //if (check)
+            //{
+            //    _httpContextAccessor.HttpContext.Response.Redirect("/login");
+            //}
+            //var authenticate = result.TryGetValue(ClaimTypes.NameIdentifier, out auth);
+
+        }
+        public IActionResult OnGet()
+        {
+            string auth;
+
+            bool check = true;
+
+            var result = _httpContextAccessor.HttpContext.Request.Cookies;
+            foreach (var cookie in result.Keys)
+            {
+                if (cookie == ".AspNetCore.cookie")
+                {
+                    check = false;
+                }
+            }
+
+            if (check)
+            {
+                return Redirect("/login");
+            }
+
+            return null;
+
         }
     }
 }
